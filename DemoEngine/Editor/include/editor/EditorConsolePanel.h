f76be9ec5332905b5831506deed4156b6ca97a64
zbiii <wszhangbei@gmail.com>
2025-07-03 18:27:49 +0800

    新增editor模块

---
+++ DemoEngine/Editor/include/editor/EditorConsolePanel.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+namespace EditorWorker
+{
+class EditorConsolePanel
+{
+    
+};
+}
---
+++ DemoEngine/Editor/include/editor/EditorManagerD3D11.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <editor/EditorProjectPanel.h>
+
+namespace EditorWorker
+{
+class EditorManagerD3D11
+{
+public:
+    EditorManagerD3D11();
+    ~EditorManagerD3D11();
+};
+}
